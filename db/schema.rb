08e0a60cd208041408744c26adec2f4b9da8aad1
Patryk Gadziński <eyelesspat@gmail.com>
2023-10-17 16:01:55 +0200

    Added Comment model with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_17_092219) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_17_112208) do
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
