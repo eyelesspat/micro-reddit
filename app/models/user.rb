89d0ded6d1fd68b2a596b5c1bc66d86cfa7e7da1
Patryk Gadziński <eyelesspat@gmail.com>
2023-10-17 12:46:14 +0200

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :gender, presence: true
 end
